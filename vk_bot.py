e44dcb4c4505d6595ad2d3f42d601309d120215f
Vladuslav Dragushin <maxhc@inbox.ru>
2023-03-13 00:33:49 +0300

    Added keyboard

--- vk_bot.py
+++ vk_bot.py
@@ -4,13 +4,28 @@ import vk_api as vk
 
 from environs import Env
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+
+def make_keyboard():
+    keyboard = VkKeyboard(one_time=True)
+
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()  # Переход на вторую строку
+    keyboard.add_button('Мой счет', color=VkKeyboardColor.POSITIVE)
+
+    return keyboard
 
 
 def echo(event, vk_api):
+    keyboard = make_keyboard()
+
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
-        random_id=random.randint(1,1000)
+        keyboard=keyboard.get_keyboard(),
+        random_id=random.randint(1, 1000)
     )
 
 
